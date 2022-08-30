7a60b9ab3f1cc7e0e615c3606cc7509b1006526d
IvanIvchik <ivanivcik11@gmaol.com>
2022-08-30 22:09:48 +0300

    feat: add physics collaps without WebAnimation API

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,37 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+"use strict"
+
+const colapsibleBtn = document.querySelector('.collapsible__button');
+const colapsibleBtnVisible = document.querySelector('.collapsible__action--visible');
+const colapsibleBtnHidden = document.querySelector('.collapsible__action--hidden');
+const collapsibleContent = document.querySelector('.collapsible__content');
+
+let isVisible = true;
+let isHidden = false;
+
+colapsibleBtnHidden.style.display = 'none';
+
+const hiddenCollapsibleContent = () => {
+    colapsibleBtnVisible.style.display = 'none';
+    collapsibleContent.style.display = 'none';
+    colapsibleBtnHidden.style.display = 'inline';
+
+    isVisible = false;
+    isHidden = true;
+}
+
+const visibleCollapsibleContent = () => {
+    colapsibleBtnVisible.style.display = 'inline';
+    collapsibleContent.style.display = 'block';
+    colapsibleBtnHidden.style.display = 'none';
+
+    isVisible = true;
+    isHidden = false;
+}
+
+const workCollapsible = () => {
+    if (isVisible === true) {
+        hiddenCollapsibleContent()
+    } else visibleCollapsibleContent()
+}
+
+colapsibleBtn.addEventListener('click', workCollapsible)
