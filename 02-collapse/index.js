e107c2b0bb735264dde6c91d2e2bf6be8f57f377
IvanIvchik <ivanivcik11@gmaol.com>
2022-08-31 20:13:59 +0300

    feat: add WebAnimation APIWebAnimation API

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,37 +1,82 @@
 "use strict"
-
-const colapsibleBtn = document.querySelector('.collapsible__button');
-const colapsibleBtnVisible = document.querySelector('.collapsible__action--visible');
-const colapsibleBtnHidden = document.querySelector('.collapsible__action--hidden');
+const collapsible = document.querySelector('.collapsible')
+const collapsibleBtn = document.querySelector('.collapsible__button');
+const collapsibleBtnVisible = document.querySelector('.collapsible__action--visible');
+const collapsibleBtnHidden = document.querySelector('.collapsible__action--hidden');
 const collapsibleContent = document.querySelector('.collapsible__content');
 
 let isVisible = true;
 let isHidden = false;
 
-colapsibleBtnHidden.style.display = 'none';
+collapsibleBtnHidden.style.display = 'none';
+collapsible.style.overflow = 'hidden';
 
-const hiddenCollapsibleContent = () => {
-    colapsibleBtnVisible.style.display = 'none';
+let animateShowContent = [
+    { transform: 'scale(0)' },
+    { color: 'red', offset: 0.6 },
+    { transform: 'scale(1)', color: 'black' }
+
+];
+
+let animateHideContent = [
+    { transform: 'scale(1)'},
+    { color: 'red', offset: 0.4 },
+    { transform: 'scale(0)', color: 'black' }
+
+];
+
+let animateClickBtn = [
+    { transform: 'scale(0.9)'},
+    { color: 'red' ,offset: 0.4 },
+    { transform: 'scale(1)', color: 'black'}
+]
+
+let animateTiming = {
+    duration: 1000,
+
+};
+
+const hideContent =() => {
     collapsibleContent.style.display = 'none';
-    colapsibleBtnHidden.style.display = 'inline';
+}
+
+const animationCollapsible = (actionWithContent) => {
+    collapsibleContent.animate(
+        actionWithContent,
+        animateTiming
+    );
+
+    collapsibleBtn.animate(
+        animateClickBtn,
+        animateTiming
+    )
+}
+
+const hideCollapsibleContent = () => {
+    animationCollapsible(animateHideContent);
+    setTimeout(hideContent, 900);
+    collapsibleBtnVisible.style.display = 'none';
+    collapsibleBtnHidden.style.display = 'inline';
 
     isVisible = false;
     isHidden = true;
 }
 
-const visibleCollapsibleContent = () => {
-    colapsibleBtnVisible.style.display = 'inline';
+const showCollapsibleContent = () => {
+    animationCollapsible(animateShowContent);
+    collapsibleBtnVisible.style.display = 'inline';
     collapsibleContent.style.display = 'block';
-    colapsibleBtnHidden.style.display = 'none';
+    collapsibleBtnHidden.style.display = 'none';
 
     isVisible = true;
     isHidden = false;
 }
 
-const workCollapsible = () => {
+const workCollapse = () => {
     if (isVisible === true) {
-        hiddenCollapsibleContent()
-    } else visibleCollapsibleContent()
+        hideCollapsibleContent();
+    } else showCollapsibleContent();
+    
 }
 
-colapsibleBtn.addEventListener('click', workCollapsible)
+collapsibleBtn.addEventListener('click', workCollapse)
